237369bea3c401327a6d6c5a449979523299b2fe
Ivonne78 <70949243+Ivonne78@users.noreply.github.com>
2021-04-12 09:42:32 +0200

    Add files via upload

---
+++ Opdracht1_Eerste_JS/index.js
@@ -0,0 +1,48 @@
+console.log('Hello Winc Academy');
+let firstName = 'Ivonne';
+let lastName = 'Den Hartog';
+let fullName = firstName + lastName;
+console.log('name');
+let number1 = 3;
+let number2 = 4;
+som = number1 + number2;
+console.log(som);
+
+function Addition() {
+    var y = 5;
+    var x = y + 2;
+
+    document.getElementById("demoAddition").innerHTML = x;
+}
+
+function Subtraction () {
+    var y = 5;
+    var x = y - 2;
+
+    document.getElementById("demoSubtraction").innerHTML = x;
+}
+
+function Multiplication () {
+    var y = 5;
+    var x = y * 2;
+
+    document.getElementById("demoMultiplication").innerHTML = x;
+}
+
+function Division () {
+    var y = 5;
+    var x = y / 2;
+
+    document.getElementById("demoDivision").innerHTML = x;
+}
+
+function Modulus () {
+    var y = 5;
+    var x = y % 2;
+
+    document.getElementById("demoModulus").innerHTML = x;
+}
+
+let age = 42;
+
+
