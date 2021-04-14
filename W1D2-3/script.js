f4d37e41de7e92fced808fd255476046de16184e
Ivonne78 <70949243+Ivonne78@users.noreply.github.com>
2021-04-14 17:46:55 +0200

    Add files via upload

---
+++ W1D2-3/script.js
@@ -0,0 +1,14 @@
+console.log("Hallo Winc Academy studenten")
+
+// "Dit is een grote som."
+/*
+let number2 = 1823794;
+let multiplication = number1 * number2;
+console.log("multiplication:", multiplication);
+
+let number3 = 637263;
+let number4 = 54;
+let division = number3 / number4;
+console.log("division:", division);*/
+
+
