878a2e5b9969d89d28a74c322a8cca05acf0ef48
Ivonne78 <70949243+Ivonne78@users.noreply.github.com>
2021-04-15 09:11:26 +0200

    Add files via upload

---
+++ Opdracht2_Gebruik_if_else_statements/script.js
@@ -0,0 +1,29 @@
+// Deel 1
+
+const Age = 22;
+
+if (Age > 18) {
+    // document.write("Je bent ouder dan 18 jaar.")
+    console.log("Je bent ouder dan 18 jaar, je mag naar binnen.")
+} else {
+    // document.write("Je bent niet ouder dan 18 jaar.")
+    console.log("Sorry, je mag er niet in. ")
+}
+
+// Deel 2
+const isFemale = true;
+
+if (isFemale) {
+    console.log("Welkom op LadiesNight")
+} else {
+    console.log("Helaas, tot de volgende MannenAvond")
+}
+
+//Deel 3
+const driverStatus = 'bob' ;
+
+if (driverStatus == 'bob') {
+    console.log("Je bent de bob, jij mag rijden vandaag.")
+} else {
+    console.log("Je mag een drankje doen, je hoeft niet te rijden vandaag.")
+}
