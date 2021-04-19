16771dbc4ff6495fadfb2548ef976134bf2ac8ea
Ivonne78 <70949243+Ivonne78@users.noreply.github.com>
2021-04-19 11:35:15 +0200

    Add files via upload

---
+++ Opdracht5_Function_basics/script.js
@@ -0,0 +1,22 @@
+const makePizza = function () {
+    console.log("Pak je telefoon");
+    console.log("Zoek een pizzeria in de buurt");
+    console.log("Kies je favoriete pizza uit");
+    console.log("Bel de pizzeria voor je bestelling.");
+}
+
+makePizza();
+makePizza();
+
+
+const makeSushi = function () {
+    console.log("Haal zalm en tonijn bij de visboer");
+    console.log("Kook de sushirijst volgens verpakking");
+    console.log("Snij de zalm en tonijn in dunne plakken")
+    console.log("Maak langwerpige balletjes van de sushirijst");
+    console.log("Leg op elk sushirijst balletje een plakej zalm of tonijn")
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
