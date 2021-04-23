c7887918af7bc058d303f8a8a9eaae539bb706da
Ivonne78 <70949243+Ivonne78@users.noreply.github.com>
2021-04-23 13:48:26 +0200

    Add files via upload

--- Guess_the_number_Game/script.js
+++ Guess_the_number_Game/script.js
@@ -2,21 +2,24 @@
 // cd Dropbox\Ivonne\Studie\Winc - JavaScript\Guess_the_number_Game
 // node script.js
 
-var firstName = window.prompt ("Welkom! Wat is je naam?");
-alert ("Hey " + firstName);
-
 // Number between 1 and 25
 var random = Math.floor(Math.random() *25) + 1;
 console.log(random);
 
+var firstName = window.prompt ("Welkom! Wat is je naam?");
+alert ("Hey " + firstName);
+
+
+
 var number = window.prompt ("Guess the number!");
 
-for (i = 1 ; number == random ; i++) {
+while (number !== random) {    
     if (number == random) {
-        alert ("Gefeliciteerd je hebt gewonnen. Het spel is nu af.")
-    } else {
-        var number = window.prompt ("Jammer, dit is niet correct. Probeer het nogmaals.");
-    }
-}
-
-alert ("Dag " + firstName + ". Tot de volgende keer.")
+        alert ("Gefeliciteerd je hebt gewonnen. Het spel is nu af.");
+        break;
+        } else { 
+            var number = window.prompt ("Jammer, dit is niet correct.");
+        }
+}      
+           
+alert ("Dag " + firstName + ". Tot de volgende keer.");
